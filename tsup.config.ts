1d13f1158237d791bc1f169dcdc6a83b995d60b1
Fabian Krupinski <fabian.krupinski@next-levels.de>
2024-12-09 09:40:05 +0100

    feat: :sparkles: Remove cjs and add minify

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entryPoints: ['src/main.ts'],
+    entry: ['src/main.ts'],
+    minify: true,
+    outDir: 'dist',
     format: ['esm'],
     dts: true,
-    outDir: 'dist',
     clean: true,
 });
