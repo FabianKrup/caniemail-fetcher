864b4998c20bdb44a153a2e7eebd887fcb6fb914
Fabian Krupinski <fabian.krupinski@next-levels.de>
2024-12-19 01:43:06 +0100

    feat: :sparkles: FInished basic fetch apiresponse nicenames and features

--- src/can-i-email-fetcher.ts
+++ src/can-i-email-fetcher.ts
@@ -1,13 +1,11 @@
-import { GithubFetchService } from './services/github-fetch.service';
-
 import type { Config } from './config';
+import { GithubFetchService } from './services/github-fetch.service';
 
 export class CanIEmailFetcher {
     private fetchService: GithubFetchService;
 
     constructor(config: Config) {
         this.fetchService = new GithubFetchService(config);
-        this.fetchService.fetchFiles();
 
         console.log('Config:', config);
     }
--- src/services/update.service.ts
+++ src/services/update.service.ts
@@ -1,9 +1,17 @@
-/*import * as fs from 'fs';
+import fm from 'front-matter';
+import * as fs from 'fs';
 import * as path from 'path';
+import { load as loadYaml } from 'js-yaml';
 
-import { GithubFetchService } from './fetch.service';
-
-import type { Config } from '../config';
+import { type Config, defaultConfig } from '../config';
+import { GithubFetchService } from './github-fetch.service';
+import {
+    FeatureTypeChecker,
+    NicenamesTypeChecker,
+    type ApiResponse,
+    type Feature,
+    type Nicenames,
+} from '../types/api-response';
 
 export class UpdateService {
     private lastUpdate: Date | null = null;
@@ -18,9 +26,9 @@ export class UpdateService {
         if (
             !this.lastUpdate ||
             now.getTime() - this.lastUpdate.getTime() >
-                this.config.updateInterval * 3600000
+                (this.config.updateInterval || defaultConfig.updateInterval) *
+                    3600000
         ) {
-            await this.fetchService.fetchFiles();
             this.lastUpdate = now;
             fs.writeFileSync(
                 path.join(__dirname, '../../data/lastUpdate.json'),
@@ -28,4 +36,68 @@ export class UpdateService {
             );
         }
     }
-}*/
+
+    private async fetchApiResponse(): Promise<ApiResponse | null> {
+        return null;
+    }
+
+    private async fetchNicenames(): Promise<Nicenames | null> {
+        const directoryContent = await this.fetchService.getContent(
+            '_data/nicenames.yml',
+        );
+
+        if (directoryContent.type === 'file' && directoryContent.content) {
+            const nicenames = loadYaml(directoryContent.content);
+
+            if (NicenamesTypeChecker.isNicenames(nicenames)) {
+                return nicenames;
+            } else {
+                console.error('Invalid nicenames data');
+                return null;
+            }
+        } else {
+            console.error('Failed to fetch nicenames');
+            return null;
+        }
+    }
+
+    private async fetchFeatures(): Promise<Feature[]> {
+        const directoryContent =
+            await this.fetchService.getContent('_features');
+
+        if (directoryContent.type === 'dir') {
+            const files = directoryContent.entries;
+            const markdownFiles = files.filter(
+                (file) =>
+                    file.type === 'file' &&
+                    file.name.endsWith('.md') &&
+                    !file.name.startsWith('_'),
+            );
+
+            const featureContents = await Promise.all(
+                markdownFiles.map(async (file) => {
+                    const content = await this.fetchService.getContent(
+                        file.path,
+                    );
+
+                    if (content.type === 'file' && content.content) {
+                        const feature = fm(content.content).attributes;
+
+                        if (FeatureTypeChecker.isFeature(feature)) {
+                            return feature;
+                        } else {
+                            console.error('Invalid feature data');
+                            return null;
+                        }
+                    }
+
+                    return null;
+                }),
+            ).then((results) => results.filter((result) => result !== null));
+
+            return featureContents;
+        }
+
+        return [];
+    }
+}
