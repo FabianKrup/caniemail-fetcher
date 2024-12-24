0f5063e10b28bddffdf052f1f6db75b27cd485cf
Fabian Krupinski <fabian.krupinski@next-levels.de>
2024-12-25 00:35:15 +0100

    feat: :sparkles: remove unused

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import { CanIEmailFetcher } from './can-i-email-fetcher';
-
 export * from './can-i-email-fetcher';
-
-const fetcher = new CanIEmailFetcher({ databasePath: '', updateInterval: 12 });
-
-fetcher.onUpdate((data) => {
-    console.log('New data received:', data);
-});
