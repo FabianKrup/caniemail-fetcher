9994909fe04df9862b26dd07868c6a9fb2c15b09
Fabian Krupinski <fabian.krupinski@next-levels.de>
2024-12-25 21:44:04 +0100

    feat: :sparkles: remove scripts

--- scripts/bump.cjs
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const { exec } = require('@actions/exec');
-
-process.chdir(path.join(__dirname, '..'));
-
-(async () => {
-    await exec('changeset', ['version']);
-})();
