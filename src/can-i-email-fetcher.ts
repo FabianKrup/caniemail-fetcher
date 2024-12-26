ba03f801c2743934d24b97603331aafb7c0528aa
Fabian Krupinski <fabian.krupinski@next-levels.de>
2024-12-26 19:50:20 +0100

    feat: :sparkles: cache current api response state and add getters

--- src/can-i-email-fetcher.ts
+++ src/can-i-email-fetcher.ts
@@ -3,25 +3,38 @@ import EventEmitter from 'events';
 import { DATA_UPDATED_EVENT, UpdateService } from './services/update.service';
 
 import type { Config } from 'config';
+import type { ApiResponse, Nicenames } from 'types/api-response';
 
 export class CanIEmailFetcher {
-    private updateService: UpdateService;
-    private eventEmitter: EventEmitter;
+    private readonly updateService: UpdateService;
+    private readonly eventEmitter: EventEmitter;
+
+    private _currentApiResponse: ApiResponse | null = null;
 
     constructor(config: Config) {
         this.updateService = new UpdateService(config);
         this.eventEmitter = new EventEmitter();
 
         this.updateService.on(DATA_UPDATED_EVENT, (data) => {
+            this._currentApiResponse = data;
+
             this.eventEmitter.emit('update', data);
         });
     }
 
-    public onUpdate(callback: (data: any) => void): void {
+    public onUpdate(callback: (data: ApiResponse) => void): void {
         this.eventEmitter.on('update', callback);
     }
 
-    public offUpdate(callback: (data: any) => void): void {
+    public offUpdate(callback: (data: ApiResponse) => void): void {
         this.eventEmitter.off('update', callback);
+    }
+
+    public get currentApiResponse(): ApiResponse | null {
+        return this._currentApiResponse;
+    }
+
+    public get currentNicenames(): Nicenames | null {
+        return this._currentApiResponse?.nicenames || null;
     }
 }
